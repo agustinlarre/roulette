4eda7014cbc5452ae6113efc7b8a7a43197a770f
agustinlarre <Agustin@Agus>
2023-11-14 23:42:40 -0300

    last one, promise...

--- src/logicaNegocio/Sesion.java
+++ src/logicaNegocio/Sesion.java
@@ -4,19 +4,15 @@
  */
 package logicaNegocio;
 
-import java.util.Date;
-
 /**
  *
  * @author agust
  */
 public class Sesion {
-    private Date horaIngreso;
     private Usuario usuario;
 
     public Sesion(Usuario usuario) {
         this.usuario = usuario;
-        this.horaIngreso = new Date();
     }
 
     public Usuario getUsuario() {
